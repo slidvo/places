9b44c09ab9a8474f4ee8e6772f42b295a206b9e9
Vyacheslav Kopeyka <ks031088@gmail.com>
2022-12-23 08:05:23 +0300

    Задание 4 Вывести тип контекста для statless и stateful виджетов

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,18 +14,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
+      home:  StatefulExample(),
+    );
+  }
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: const StatefulExample(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      title: "TITLE APP!!!!",
     );
   }
 }
@@ -121,22 +127,26 @@ class MyFirstWidget extends StatelessWidget {
 
   const MyFirstWidget({super.key});
 
+
+
   @override
   Widget build(BuildContext context) {
+      String getContextRuntimeType(){
+    return context.runtimeType.toString();
+  } 
     int counter = 0;
     counter++;
     print("Метод build был вызван counter=$counter");
+    print("Context runtimeType ${getContextRuntimeType()}");
     
-    return Container(
-      child: const Center(
+    return const Center(
         child: Text('Hello!')
-      ),
-    );
+      );
   }
 }
 
 class StatefulExample extends StatefulWidget {
-  const StatefulExample({super.key});
+   const StatefulExample({super.key});
 
   @override
   State<StatefulExample> createState() => _StatefulExampleState();
@@ -151,8 +161,15 @@ class _StatefulExampleState extends State<StatefulExample> {
   }
   @override
   Widget build(BuildContext context) {
+       String getContextRuntimeType(){
+    return context.runtimeType.toString();
+  } 
     _count();
     print("counter = $counter");
-    return Container();
+    print("Context runtimeType ${getContextRuntimeType()}");
+    
+    return  const Center(
+        child: Text('Hello!')
+      );
   }
 }
