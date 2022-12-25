919e28c02a33ddabb23f0066d417573cb238ad06
Vyacheslav Kopeyka <38747054+slidvo@users.noreply.github.com>
2022-12-25 14:18:52 +0300

    M4L6T2: Задание 2 Верстка списка интересных мест

--- lib/mocks.dart
+++ lib/mocks.dart
@@ -46,22 +46,20 @@ final List<Sight> mocks = [
         "Ханты-Мансийском автономном округе.",
   ),
   Sight(
-    name: "Секретная база США",
-    lat: 32.664162,
-    lon: -111.487119,
-    url:
-        "http://img.gazeta.ru/files3/225/10709225/Al_Udeid_Air_Base-pic905-895x505-48956.jpg",
-    details:
-        "Раскрыта секретная база США, на которой изучали внеземные технологии",
-    type: "SECRET",
-  ),
+      name: "Секретная база США",
+      lat: 32.664162,
+      lon: -111.487119,
+      url:
+          "https://img.gazeta.ru/files3/225/10709225/Al_Udeid_Air_Base-pic905-895x505-48956.jpg",
+      details:
+          "Раскрыта секретная база США, на которой изучали внеземные технологии",
+      type: "SECRET"),
   Sight(
-    name: "Самолёт среди деревье",
-    lat: 45.408166,
-    lon: -123.008118,
-    url:
-        "https://img-fotki.yandex.ru/get/5009/19099752.40/0_a0540_adae51e5_XL.jpg",
-    type: "MYSTIC",
-    details: "Самолёт в джунглях!!!",
-  )
+      name: "Самолёт в джунглях",
+      lat: 45.408166,
+      lon: -123.008118,
+      url:
+          "https://img-fotki.yandex.ru/get/5009/19099752.40/0_a0540_adae51e5_XL.jpg",
+      type: "MYSTIC",
+      details: "Просто пассажирский самолёт в джунглях")
 ];
