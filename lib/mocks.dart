f09fce1a2969b4a2c325e5a64e84aac4774c954b
Vyacheslav Kopeyka <38747054+slidvo@users.noreply.github.com>
2022-12-25 08:10:37 +0300

    M4L6: Задание 1 Создать Моки интересных мест

---
+++ lib/mocks.dart
@@ -0,0 +1,21 @@
+import 'domain/sight.dart';
+
+final List<Sight> mocks = [
+  Sight(
+      name: "Секретная база США",
+      lat: 32.664162,
+      lon: -111.487119,
+      url:
+          "https://img.gazeta.ru/files3/225/10709225/Al_Udeid_Air_Base-pic905-895x505-48956.jpg",
+      details:
+          "Раскрыта секретная база США, на которой изучали внеземные технологии",
+      type: "SECRET"),
+  Sight(
+      name: "Самолёт среди деревье",
+      lat: 45.408166,
+      lon: -123.008118,
+      url:
+          "https://img-fotki.yandex.ru/get/5009/19099752.40/0_a0540_adae51e5_XL.jpg",
+      type: "MYSTIC",
+      details: "Самолёт в джунглях!!!")
+];
