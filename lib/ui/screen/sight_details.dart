55b8cf87c5876086dc3428ef3b203d12b0f7eed3
Vyacheslav Kopeyka <38747054+slidvo@users.noreply.github.com>
2022-12-27 21:48:21 +0300

    M4L6T3: Модуль 4. Урок 6. Задание 3 Скроллить только описание интересного места.

--- lib/ui/screen/sight_details.dart
+++ lib/ui/screen/sight_details.dart
@@ -55,203 +55,205 @@ class SightDetails extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
+            child:  Container(
+              margin: EdgeInsets.only(top: 30),
               child: Container(
-                margin: EdgeInsets.only(top: 30),
-                child: Container(
-                  margin: EdgeInsets.fromLTRB(16, 0, 16, 16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        sight.name,
-                        style: const TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w700,
-                          fontStyle: FontStyle.normal,
-                          fontFamily: "Roboto",
-                          height: 1.2,
-                          color: Color.fromRGBO(59, 62, 91, 1),
-                          decoration: TextDecoration.none,
-                        ),
-                        textAlign: TextAlign.left,
+                margin: EdgeInsets.fromLTRB(16, 0, 16, 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      sight.name,
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.normal,
+                        fontFamily: "Roboto",
+                        height: 1.2,
+                        color: Color.fromRGBO(59, 62, 91, 1),
+                        decoration: TextDecoration.none,
                       ),
-                      Row(
-                        children: [
-                          Text(
-                            sight.type,
-                            style: const TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w700,
-                              fontStyle: FontStyle.normal,
-                              fontFamily: "Roboto",
-                              height: 1.29,
-                              color: Color.fromRGBO(59, 62, 91, 1),
-                              decoration: TextDecoration.none,
-                            ),
+                      textAlign: TextAlign.left,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          sight.type,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700,
+                            fontStyle: FontStyle.normal,
+                            fontFamily: "Roboto",
+                            height: 1.29,
+                            color: Color.fromRGBO(59, 62, 91, 1),
+                            decoration: TextDecoration.none,
                           ),
-                          const Divider(
-                            indent: 16,
+                        ),
+                        const Divider(
+                          indent: 16,
+                        ),
+                        const Text(
+                          "закрыто до 09:00",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            fontStyle: FontStyle.normal,
+                            fontFamily: "Roboto",
+                            height: 1.29,
+                            color: Color.fromRGBO(59, 62, 91, 1),
+                            decoration: TextDecoration.none,
                           ),
-                          const Text(
-                            "закрыто до 09:00",
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                              fontStyle: FontStyle.normal,
-                              fontFamily: "Roboto",
-                              height: 1.29,
-                              color: Color.fromRGBO(59, 62, 91, 1),
-                              decoration: TextDecoration.none,
-                            ),
+                        ),
+                      ],
+                    ),
+                    const Divider(
+                      height: 24,
+                      color: Colors.transparent,
+                    ),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Text(
+                          sight.details,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            fontStyle: FontStyle.normal,
+                            fontFamily: "Roboto",
+                            height: 1.2,
+                            color: Color.fromRGBO(59, 62, 91, 1),
+                            decoration: TextDecoration.none,
                           ),
-                        ],
-                      ),
-                      const Divider(
-                        height: 24,
-                        color: Colors.transparent,
-                      ),
-                      Text(
-                        sight.details,
-                        style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.normal,
-                          fontFamily: "Roboto",
-                          height: 1.2,
-                          color: Color.fromRGBO(59, 62, 91, 1),
-                          decoration: TextDecoration.none,
                         ),
                       ),
-                      const Divider(
-                        height: 24,
-                        color: Colors.transparent,
-                      ),
-                      Container(
-                        height: 48,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          color: const Color.fromRGBO(76, 175, 80, 1),
-                        ),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              width: 20,
-                              height: 22,
-                              color: Colors.white,
-                            ),
-                            Divider(
-                              color: Colors.transparent,
-                              indent: 16,
-                            ),
-                            const Text(
-                              "ПОСТРОИТЬ МАРШРУТ",
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w700,
-                                fontStyle: FontStyle.normal,
-                                fontFamily: "Roboto",
-                                height: 1.29,
-                                color: Colors.white,
-                                decoration: TextDecoration.none,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      const Divider(height: 24),
-                      Container(
-                        height: 40,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Row(
-                                  children: [
-                                    Icon(
-                                      Icons.calendar_month,
-                                      color: Color.fromRGBO(124, 126, 146, 0.56),
-                                      size: 24,
-                                    ),
-                                    Divider(
-                                      indent: 9,
-                                    ),
-                                    Text(
-                                      "Запланировать",
-                                      style: const TextStyle(
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.w400,
-                                        fontStyle: FontStyle.normal,
-                                        fontFamily: "Roboto",
-                                        height: 1.2,
-                                        color:
-                                            Color.fromRGBO(124, 126, 146, 0.56),
-                                        decoration: TextDecoration.none,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ],
-                            ),
-                            Divider(
-                              indent: 16,
-                            ),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Row(
-                                  children: const [
-                                    Icon(
-                                      Icons.favorite_border,
-                                    ),
-                                    Divider(
-                                      indent: 9,
-                                    ),
-                                    Text(
-                                      "В избранное",
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.w400,
-                                        fontStyle: FontStyle.normal,
-                                        fontFamily: "Roboto",
-                                        height: 1.2,
-                                        color: Color.fromRGBO(59, 62, 91, 1),
-                                        decoration: TextDecoration.none,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                      Divider(
-                        height: 24,
-                        color: Colors.transparent,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 10,
-                            height: 2,
-                            color: Color.fromRGBO(217, 217, 217, 1),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
+          const Divider(
+            height: 24,
+            color: Colors.transparent,
+          ),
+          Container(
+            height: 48,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: const Color.fromRGBO(76, 175, 80, 1),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 20,
+                  height: 22,
+                  color: Colors.white,
+                ),
+                Divider(
+                  color: Colors.transparent,
+                  indent: 16,
+                ),
+                const Text(
+                  "ПОСТРОИТЬ МАРШРУТ",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.normal,
+                    fontFamily: "Roboto",
+                    height: 1.29,
+                    color: Colors.white,
+                    decoration: TextDecoration.none,
+                  ),
+                )
+              ],
+            ),
+          ),
+          const Divider(height: 24),
+          Container(
+            height: 40,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.calendar_month,
+                          color: Color.fromRGBO(124, 126, 146, 0.56),
+                          size: 24,
+                        ),
+                        Divider(
+                          indent: 9,
+                        ),
+                        Text(
+                          "Запланировать",
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            fontStyle: FontStyle.normal,
+                            fontFamily: "Roboto",
+                            height: 1.2,
+                            color:
+                            Color.fromRGBO(124, 126, 146, 0.56),
+                            decoration: TextDecoration.none,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                Divider(
+                  indent: 16,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: const [
+                        Icon(
+                          Icons.favorite_border,
+                        ),
+                        Divider(
+                          indent: 9,
+                        ),
+                        Text(
+                          "В избранное",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            fontStyle: FontStyle.normal,
+                            fontFamily: "Roboto",
+                            height: 1.2,
+                            color: Color.fromRGBO(59, 62, 91, 1),
+                            decoration: TextDecoration.none,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Divider(
+            height: 24,
+            color: Colors.transparent,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 10,
+                height: 2,
+                color: Color.fromRGBO(217, 217, 217, 1),
+              ),
+            ],
+          ),
+
         ],
       ),
     );
